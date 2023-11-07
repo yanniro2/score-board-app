4c32e49ce5b7e6737116f67c56121d5ce2b254a1
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-11-07 11:24:00 +0530

    ad ui dynamic add

--- components/AdvertismentDashboard.tsx
+++ components/AdvertismentDashboard.tsx
@@ -6,15 +6,37 @@ import { LuPanelBottom } from "react-icons/lu";
 
 const AdvertismentDashboard = () => {
   const options = [
-    { label: "Default", value: "item1", icon: <BiGrid /> },
-    { label: "Bottom", value: "item2", icon: <LuPanelBottom /> },
-    { label: "Full Screen", value: "item3", icon: <BiFullscreen /> },
-    { label: "3/4 Screen", value: "item4", icon: <BiLayout /> },
+    {
+      id: 1,
+      heading: "Advertisements",
+      data: [
+        { label: "Default", value: "ad1", icon: <BiGrid /> },
+        { label: "Bottom", value: "ad2", icon: <LuPanelBottom /> },
+        { label: "Full Screen", value: "ad3", icon: <BiFullscreen /> },
+        { label: "3/4 Screen", value: "ad4", icon: <BiLayout /> },
+      ],
+    },
+    {
+      id: 2,
+      heading: "Sponsors",
+      data: [
+        { label: "Default", value: "sponsor1", icon: <BiGrid /> },
+        { label: "Bottom", value: "sponsor2", icon: <LuPanelBottom /> },
+        { label: "Full Screen", value: "sponsor3", icon: <BiFullscreen /> },
+        { label: "3/4 Screen", value: "sponsor4", icon: <BiLayout /> },
+      ],
+    },
   ];
 
   return (
     <div className="w-1/2 h-max mx-auto  flex flex-col justify-between gap-[2rem]">
-      <RadioGroup options={options} />;
+      {options.map((option) => (
+        <RadioGroup
+          key={option.id}
+          options={option.data}
+          heading={option.heading}
+        />
+      ))}
     </div>
   );
 };
