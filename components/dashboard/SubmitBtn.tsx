f44f5cfb41a8255bcec6fb0ce4eac969ab0e5104
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-11-19 00:57:38 +0530

    Fix submit button

--- components/dashboard/SubmitBtn.tsx
+++ components/dashboard/SubmitBtn.tsx
@@ -1,24 +1,35 @@
 "use client";
-import React, { useState } from "react";
-import { MdOutlineChangeCircle } from "react-icons/md";
+import { useState } from "react";
 import { useFormStatus } from "react-dom";
+import { MdOutlineChangeCircle } from "react-icons/md";
 import { IoCheckmarkDoneCircle } from "react-icons/io5";
 const SubmitBtn = () => {
   const { pending } = useFormStatus();
+
   const [start, setStart] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setStart(true);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  };
+
   return (
     <button
       type="submit"
       className="bg-white py-3 px-6 font-semibold text-black  transition absolute top-6 right-6 rounded-xl flex items-center justify-center gap-[1rem] drop-shadow shadow-md m-1 "
       disabled={pending}
-      onClick={() => setStart(true)}>
+      onClick={handleClick}>
       {pending ? (
-        <div className="text-red-500 flex items-center justify-center gap-[1rem] ">
+        <div className="text-blue-500 flex items-center justify-center gap-[1rem] ">
           {start ? (
-            <div>Checking....</div>
+            <div className="flex items-center justify-center gap-[1rem]">
+              Checking.... <MdOutlineChangeCircle className="text-[1.5rem]" />
+            </div>
           ) : (
             <div className="flex items-center justify-center gap-[1rem]">
-              Check <MdOutlineChangeCircle className="text-[1.5rem]" />
+              Click to Add
             </div>
           )}
         </div>
@@ -26,12 +37,12 @@ const SubmitBtn = () => {
         <div className="flex items-center justify-center gap-[1rem] ">
           {start ? (
             <div className="flex items-center justify-center gap-[1rem]">
-              Update
+              Added Success
               <IoCheckmarkDoneCircle className="text-green-600 text-[1.5rem]" />
             </div>
           ) : (
             <div className="flex items-center justify-center gap-[1rem]">
-              Check <MdOutlineChangeCircle className="text-[1.5rem]" />
+              Click to Add <MdOutlineChangeCircle className="text-[1.5rem]" />
             </div>
           )}
         </div>
